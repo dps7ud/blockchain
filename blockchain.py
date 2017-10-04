8dda70594a327d0cfd3eccf1d4a83aa3ffb4dcd3
dps7ud <dps7ud@virginia.edu>
2017-10-04 18:20:30 -0400

    blockchain test-ready

--- blockchain.py
+++ blockchain.py
@@ -5,7 +5,7 @@ from textwrap import dedent
 from time import time
 from uuid import uuid4
 
-from flask import Flask
+from flask import Flask, jsonify, request
 
 
 
@@ -104,3 +104,56 @@ class Blockchain(object):
 app = Flask(__name__)
 node_identifier = str(uuid4()).replace('-', '')
 blockchain = Blockchain()
+
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    last_block = blockchain.last_block
+    last_proof = last_block['proof']
+    proof = blockchain.proof_of_work(last_proof)
+
+    blockchain.new_transaction(
+                sender='0',
+                recipient=node_identifier,
+                amount=1
+            )
+    block=blockchain.new_block(proof)
+
+    response = {
+                'message': "New block",
+                'index': block['index'],
+                'transactions': block{'transactions'],
+                'proof': block['proof']
+                'previous_hash': block['previous_hash'],
+            }
+    return jsonify(response), 200
+
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    request_data = request.get_json()
+    print(request_data)
+    required = {'sender', 'recipient', 'amount'}
+    missing = required - request_data.keys()
+    if missing:
+        error_message = "Missing values: " + ', '.join(missing)
+        return error_message, 400
+    index = blockchain.new_transaction(
+            request_data['sender'],
+            request_data['recipient'],
+            request_data['amount'])
+    response = {'message': "Transaction will be added to Block {}".format(index)}
+    return jsonify(response), 201
+
+
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+                'chain': blockchain.chain,
+                'length': len(blockchain.chian)
+            }
+    return jsonify(response), 200
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
